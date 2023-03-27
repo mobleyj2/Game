7cf2a9d27ad4edd34006ed19d49ff2e9fb3584e5
mobleyj2 <122937476+mobleyj2@users.noreply.github.com>
2023-03-27 12:35:35 -0400

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,18 @@
+
+
+//start button
+
+//Timer
+
+//question with diffrent answers
+
+//new question
+
+//question wrong takes time off
+
+//when time reaches 0 
+
+//game over
+
+//save name and score
+
