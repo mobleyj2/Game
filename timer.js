0b6c1ff2ba2f9bdaa041e484a0f4d0edd1106dc5
mobleyj2 <122937476+mobleyj2@users.noreply.github.com>
2023-03-29 18:10:20 -0400

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,18 +1,80 @@
+let startBtnEl =  document.getElementById("start-btn");
+startBtnEl.addEventListener("click", startTheQuiz);
+let startPageDivEl = document.getElementById("start-page-div");
+let timeLeft = 20;
+let timerId;
+let timeEl = document.getElementById("time");
+let questionsPageDivEl =  document.getElementById("questions-page-div");
 
+let questions = [
+  { question: "Q1",
+    choices: ["A1",  "B1", "C1", "D1"],
+    corrAns: "C1"
+  },
+  { question: "Q2",
+  choices: ["A2",  "B2", "C2", "D2"],
+  corrAns: "C2"
+},
+{ question: "Q3",
+choices: ["A3",  "B3", "C3", "D3"],
+corrAns: "B3"
+},
+{ question: "Q4",
+choices: ["A4",  "B4", "C4", "D4"],
+corrAns: "A4"
+},
+{ question: "Q5",
+choices: ["A5",  "B5", "C5", "D5"],
+corrAns: "D5"
+},
+{ question: "Q6",
+choices: ["A6",  "B6", "C6", "D6"],
+corrAns: "A6"
+},
+];
 
-//start button
+let index =0;
+let currentQuestion = questions[index];
+let choicesEl = document.getElementById("choices");
 
-//Timer
+function startTheQuiz() {
+  // start page handling
+startPageDivEl.setAttribute("class", "hide");
 
-//question with diffrent answers
+// timer handling
+timerId = setInterval (OneSecTimerFunc,1000);
+timeEl.textContent =  timeLeft;
 
-//new question
+// questions page handling
+questionsPageDivEl.removeAttribute("class");
+showQuestion();
+}
 
-//question wrong takes time off
+function OneSecTimerFunc() {
+  timeLeft -=1;  // timeLeft--;
+  timeEl.textContent =  timeLeft;
 
-//when time reaches 0 
+}
 
-//game over
+function showQuestion() {
+  
+currentQuestion = questions[index];
+let questionEl = document.getElementById("question");
 
-//save name and score
+questionEl.textContent = currentQuestion.question;
 
+choicesEl.innerHTML = "";
+
+for (let i=0; i <currentQuestion.choices.length; i++){
+
+  let choice =  currentQuestion.choices[i];
+  let choiceBtnEl = document.createElement('button');
+  choiceBtnEl.setAttribute('class','choice');
+  choiceBtnEl.setAttribute('value',  choice);
+  choiceBtnEl.textContent = i + 1 + '. ' + choice;
+  choicesEl.appendChild(choiceBtnEl);
+ 
+
+}
+
+}
